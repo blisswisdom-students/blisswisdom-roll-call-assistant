a5fc7ebaff037dbb5d842b546a38c7b965b50a95
Johann Chang <mr.changyuheng@gmail.com>
2023-04-05 10:25:55 +0800

    Support to build executable binary

--- packages/blisswisdom_roll_call_assistant_desktop/ui/main_window.py
+++ packages/blisswisdom_roll_call_assistant_desktop/ui/main_window.py
@@ -1,9 +1,10 @@
 from __future__ import annotations
 
 import importlib.resources
+import pathlib
 
 import PySide6.QtXml  # This is only for PyInstaller to process properly
-from PySide6.QtGui import QCloseEvent, QPaintEvent, QPixmap
+from PySide6.QtGui import QCloseEvent, QIcon, QPaintEvent, QPixmap
 from PySide6.QtWidgets import (
     QMainWindow)
 
@@ -15,6 +16,11 @@ class QMainWindowExt(QMainWindow):
         super().paintEvent(event)
         self.setWindowTitle(f'{sdk.PROG_NAME} {sdk.VERSION}')
 
+        app_icon: pathlib.Path
+        with importlib.resources.path(__package__, 'icon.ico') as app_icon:
+            self.setWindowIcon(QIcon(str(app_icon)))
+
+        banner_path: pathlib.Path
         with importlib.resources.path(__package__, 'banner.png') as banner_path:
             self.banner_label.setPixmap(QPixmap(banner_path))
 

78825a60762c58e991a8e82b5386c99a4217e56b
Johann Chang <mr.changyuheng@gmail.com>
2023-04-05 12:17:25 +0800

    Support to report issues

--- packages/blisswisdom_roll_call_assistant_desktop/ui/main_window.py
+++ packages/blisswisdom_roll_call_assistant_desktop/ui/main_window.py
@@ -2,10 +2,12 @@ from __future__ import annotations
 
 import importlib.resources
 import pathlib
+import webbrowser
 
-import PySide6.QtXml  # This is only for PyInstaller to process properly
+from PySide6.QtCore import Qt
 from PySide6.QtGui import QCloseEvent, QIcon, QPaintEvent, QPixmap
 from PySide6.QtWidgets import QLabel, QLineEdit, QMainWindow, QPlainTextEdit, QPushButton
+import PySide6.QtXml  # This is only for PyInstaller to process properly
 
 import blisswisdom_roll_call_assistant_sdk as sdk
 
@@ -25,21 +27,25 @@ class QMainWindowExt(QMainWindow):
         self.status_plain_text_edit: QPlainTextEdit = getattr(self, 'status_plain_text_edit')
 
         self.edit_push_button: QPushButton = getattr(self, 'edit_push_button')
-        self.edit_push_button.clicked.connect(self.on_edit_push_button_clicked)
+        self.edit_push_button.clicked.connect(self.on_edit_push_button_clicked, type=Qt.UniqueConnection)
 
         self.save_push_button: QPushButton = getattr(self, 'save_push_button')
-        self.save_push_button.clicked.connect(self.on_save_push_button_clicked)
+        self.save_push_button.clicked.connect(self.on_save_push_button_clicked, type=Qt.UniqueConnection)
 
         self.cancel_push_button: QPushButton = getattr(self, 'cancel_push_button')
-        self.cancel_push_button.clicked.connect(self.on_cancel_push_button_clicked)
+        self.cancel_push_button.clicked.connect(self.on_cancel_push_button_clicked, type=Qt.UniqueConnection)
 
         self.start_push_button: QPushButton = getattr(self, 'start_push_button')
+        self.start_push_button.clicked.connect(self.on_start_push_button_clicked, type=Qt.UniqueConnection)
 
         self.stop_push_button: QPushButton = getattr(self, 'stop_push_button')
+        self.stop_push_button.clicked.connect(self.on_stop_push_button_clicked, type=Qt.UniqueConnection)
 
         self.log_in_push_button: QPushButton = getattr(self, 'log_in_push_button')
+        self.log_in_push_button.clicked.connect(self.on_log_in_push_button_clicked, type=Qt.UniqueConnection)
 
         self.help_push_button: QPushButton = getattr(self, 'help_push_button')
+        self.help_push_button.clicked.connect(self.on_help_push_button_clicked, type=Qt.UniqueConnection)
 
         app_icon: pathlib.Path
         with importlib.resources.path(__package__, 'icon.ico') as app_icon:
@@ -64,6 +70,12 @@ class QMainWindowExt(QMainWindow):
         self.start_push_button.setEnabled(not enabled)
         self.log_in_push_button.setEnabled(not enabled)
 
+    def set_in_progress(self, in_progress: bool) -> None:
+        self.edit_push_button.setEnabled(not in_progress)
+        self.start_push_button.setEnabled(not in_progress)
+        self.stop_push_button.setEnabled(in_progress)
+        self.log_in_push_button.setEnabled(not in_progress)
+
     def on_edit_push_button_clicked(self) -> None:
         self.set_edit_enabled(True)
 
@@ -72,3 +84,15 @@ class QMainWindowExt(QMainWindow):
 
     def on_cancel_push_button_clicked(self) -> None:
         self.set_edit_enabled(False)
+
+    def on_start_push_button_clicked(self) -> None:
+        self.set_in_progress(True)
+
+    def on_stop_push_button_clicked(self) -> None:
+        self.set_in_progress(False)
+
+    def on_log_in_push_button_clicked(self) -> None:
+        self.set_in_progress(True)
+
+    def on_help_push_button_clicked(self) -> None:
+        webbrowser.open('https://github.com/changyuheng/blisswisdom-roll-call-assistant/issues')

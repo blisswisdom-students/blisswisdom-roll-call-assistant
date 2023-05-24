415cc843047b24d38e37fda30ad15e3c9b7889b7
Johann Chang <johann@lumo.ag>
2023-05-24 11:17:25 -0700

    Fix not able to get the application location on macOS issue

--- packages/blisswisdom_roll_call_assistant_sdk/util.py
+++ packages/blisswisdom_roll_call_assistant_sdk/util.py
@@ -1,23 +1,20 @@
 import pathlib
+import platform
+import sys
 
-import xdg_base_dirs
-
-from .constant import PROG_NAME
+import macos_application_location
 
 
 def get_cache_dir() -> pathlib.Path:
-    path: pathlib.Path = xdg_base_dirs.xdg_cache_home() / PROG_NAME
-    path.mkdir(parents=True, exist_ok=True)
-    return path
+    return macos_application_location.get().parent if platform.system() == 'Darwin' else \
+        (pathlib.Path.cwd() / sys.argv[0]).parent
 
 
 def get_config_dir() -> pathlib.Path:
-    path: pathlib.Path = xdg_base_dirs.xdg_config_home() / PROG_NAME
-    path.mkdir(parents=True, exist_ok=True)
-    return path
+    return macos_application_location.get().parent if platform.system() == 'Darwin' else \
+        (pathlib.Path.cwd() / sys.argv[0]).parent
 
 
 def get_data_dir() -> pathlib.Path:
-    path: pathlib.Path = xdg_base_dirs.xdg_data_home() / PROG_NAME
-    path.mkdir(parents=True, exist_ok=True)
-    return path
+    return macos_application_location.get().parent if platform.system() == 'Darwin' else \
+        (pathlib.Path.cwd() / sys.argv[0]).parent
